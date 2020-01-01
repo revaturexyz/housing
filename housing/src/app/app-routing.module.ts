c42859ae88060497b11fa48ac078bce5d7ad1fc1
han <han.nguyen@outlook.com>
2020-01-01 13:30:18 -0600

    switch from AuthGuard to OktaAuthGuard

--- housing/src/app/app-routing.module.ts
+++ housing/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CoordinatorNotificationsComponent } from './coordinator-notifications/coordinator-notifications.component';
-import { AuthGuard } from './guards/auth.guard';
 import { AssignTenantToRoomComponent } from './assign-tenant-to-room/assign-tenant-to-room.component';
 
 import { EditProviderComponent } from './edit-provider/edit-provider.component';
@@ -11,7 +10,7 @@ import { ManageComplexComponent } from './manage-complex/manage-complex.componen
 import { SearchTenantComponent } from './search-tenant/search-tenant.component';
 import { SelectTenantComponent } from './select-tenant/select-tenant.component';
 import { AddTenantComponent } from './add-tenant/add-tenant.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
 
 const routes: Routes = [
   // dev-assign-tenant-to-room
@@ -21,10 +20,10 @@ const routes: Routes = [
   { path: 'search-tenant', component: SearchTenantComponent },
   { path: 'select-tenant/:id', component: SelectTenantComponent },
   { path: '', component: HomeComponent },
-  { path: 'coordinator-notifications', component: CoordinatorNotificationsComponent, canActivate: [AuthGuard] },
-  { path: 'edit-provider', component: EditProviderComponent, canActivate: [AuthGuard] },
-  { path: 'provider-status', component: ProviderStatusComponent, canActivate: [AuthGuard] },
-  { path: 'manage-complex', component: ManageComplexComponent, canActivate: [AuthGuard] },
+  { path: 'coordinator-notifications', component: CoordinatorNotificationsComponent, canActivate: [OktaAuthGuard] },
+  { path: 'edit-provider', component: EditProviderComponent, canActivate: [OktaAuthGuard] },
+  { path: 'provider-status', component: ProviderStatusComponent, canActivate: [OktaAuthGuard] },
+  { path: 'manage-complex', component: ManageComplexComponent, canActivate: [OktaAuthGuard] },
   { path: 'add-tenant', component: AddTenantComponent },
   { path: 'implicit/callback', component: OktaCallbackComponent }
 ];
