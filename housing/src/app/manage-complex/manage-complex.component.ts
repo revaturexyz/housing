622ea895e8b5dbba51f7837a43188031b3205ea6
lyndsay-durbin <lyndsaydurbin@yahoo.com>
2020-01-02 15:49:52 -0600

    add dummy complex in

--- housing/src/app/manage-complex/manage-complex.component.ts
+++ housing/src/app/manage-complex/manage-complex.component.ts
@@ -4,6 +4,11 @@ import { FormControl } from '@angular/forms';
 // import { TestServiceData } from 'src/app/services/static-test-data';
 import { Room } from 'src/interfaces/room';
 
+import { Address } from 'src/interfaces/address';
+import { Amenity } from 'src/interfaces/amenity';
+import { Provider} from 'src/interfaces/account/provider';
+import { Status} from 'src/interfaces/account/status';
+
 @Component({
   selector: 'dev-manage-complex',
   templateUrl: './manage-complex.component.html',
@@ -13,11 +18,50 @@ import { Room } from 'src/interfaces/room';
 // Component used to handle logic behind selecting and managing a complex
 export class ManageComplexComponent implements OnInit {
 
+  public dummyAmenity: Amenity = {
+    amenityId: 1,
+    amenity: "pool",
+    isSelected: true,
+  };
+
+  public dummyAddress: Address = {
+    addressId: 1,
+    streetAddress: '123 Sesame St',
+    city: 'Arlington',
+    state: 'TX',
+    zipcode: '12345'
+  };
+
+  public dummyStatus: Status = {
+    statusText: 'approved'
+  }
+
+  public dummyProvider: Provider  = {
+    providerId: 1,
+    coordinatorId: '1',
+    name: 'john',
+    email: 'john@email.com',
+    status: this.dummyStatus,
+    accountCreatedAt: new Date(),
+    accountExpiresAt: new Date(),
+  };
+
+  public dummyComplex: Complex = {
+    complexId: 1,
+    apiAddress: this.dummyAddress,
+    apiProvider: this.dummyProvider,
+    complexName: 'liv+',
+    contactNumber: '1234567890',
+    amenity: [this.dummyAmenity],
+  };
+
   public seededComplexes: Complex[] = [
     // TestServiceData.dummyComplex,
     // TestServiceData.dummyComplex2,
     // TestServiceData.dummyComplex,
     // TestServiceData.dummyComplex2
+    
+    
   ];
 
   // mode selection =>
