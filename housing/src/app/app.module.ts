ec0304dba511bdd0af451d4605e79eb1d1265da5
lyndsay-durbin <lyndsaydurbin@yahoo.com>
2020-01-07 13:17:31 -0600

    fix imports for merge conflict

--- housing/src/app/app.module.ts
+++ housing/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { StickyNavModule } from 'ng2-sticky-nav';
 import {
   MatChipsModule, MatTableModule, MatDialogModule, MatPaginatorModule, MatFormFieldModule,
   MatSelectModule, MatSidenavModule, MatIconModule, MatButtonModule, MatDividerModule, MatListModule,
-  MatExpansionModule, MatInputModule, MatToolbar, MatToolbarModule
+  MatExpansionModule, MatInputModule, MatToolbarModule, MatGridListModule
 } from '@angular/material';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CdkTableModule } from '@angular/cdk/table';
@@ -95,6 +95,7 @@ import { AddTenantComponent } from './add-tenant/add-tenant.component';
     MatInputModule,
     MatCheckboxModule,
     ScrollingModule,
+    MatGridListModule
   ],
   providers: [
     {
