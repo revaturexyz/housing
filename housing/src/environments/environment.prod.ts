7d36f4ffca02ca42ec4849f3db3c0ffde3f419f9
han <han.nguyen@outlook.com>
2020-01-08 12:48:27 -0600

    alter environment.prod for redirect

--- housing/src/environments/environment.prod.ts
+++ housing/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   domain: 'https://dev-837913.okta.com/oauth2/default', // Absent trailing slash matters here
   clientID: '0oa2d72hlcH7CUgwf357',
-  redirectUri: 'http://localhost:9000/implicit/callback',
+  redirectUri: 'http://account.southcentralus.cloudapp.azure.com/implicit/callback',
   audience: '/account',
   claimsDomain: 'https://revature.com/', // Trailing slash matters here
   googleMapsKey: 'AIzaSyCxYMcmEjlHQ2r2CywMgyK7YEplxurqW2A',
