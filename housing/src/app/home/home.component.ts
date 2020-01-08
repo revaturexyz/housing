0bb18b9acf2aa3ab419f595177edac380a949938
han <han.nguyen@outlook.com>
2020-01-08 15:27:36 -0600

    Change Register to Login

--- housing/src/app/home/home.component.ts
+++ housing/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Provider } from 'src/interfaces/account/provider';
 import { Complex } from 'src/interfaces/complex';
+import { UserService } from '../services/user.service';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'dev-home',
@@ -13,10 +15,22 @@ export class HomeComponent implements OnInit {
   roomList: object;
   provider: Provider;
   complexes: Complex[];
+  public role: string;
+  public isAuthenticated: boolean;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public user: UserService, public oktaAuth: OktaAuthService) {
+    // get authentication state for immediate use
+    this.oktaAuth.isAuthenticated().then(result => {
+      this.isAuthenticated = result;
+    });
+
+    // subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+    );
+
+    user.Roles$.subscribe(res => this.role = res[1]); // index 0 is 'Everyone', index 1 is the actual role.
   }
-
   ngOnInit() {
   }
 }
