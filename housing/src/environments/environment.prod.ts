0c7a98b1ff8899508fd3ebf376ca9e8e8a979861
han <han.nguyen@outlook.com>
2020-01-08 20:02:39 -0600

    fixed azure links

--- housing/src/environments/environment.prod.ts
+++ housing/src/environments/environment.prod.ts
@@ -2,18 +2,14 @@ export const environment = {
   production: true,
   domain: 'https://dev-837913.okta.com/oauth2/default', // Absent trailing slash matters here
   clientID: '0oa2d72hlcH7CUgwf357',
-  redirectUri: 'http://account.southcentralus.cloudapp.azure.com/implicit/callback',
+  redirectUri: 'https://housing-angular-dev.azurewebsites.net/implicit/callback',
   audience: '/account',
   claimsDomain: 'https://revature.com/', // Trailing slash matters here
   googleMapsKey: 'AIzaSyCxYMcmEjlHQ2r2CywMgyK7YEplxurqW2A',
   endpoints: {
-    // account: 'https://account-aspnet-dev.azurewebsites.net/',
-    // complex: 'https://complex-aspnet-dev.azurewebsites.net/',
-    // tenant: 'https://tenant-aspnet-dev.azurewebsites.net/',
-    // provider: 'http://localhost:10080/'
-    account: 'http://account.southcentralus.cloudapp.azure.com/',
-    complex: 'http://complex.southcentralus.cloudapp.azure.com/',
-    tenant: 'http://tenant.southcentralus.cloudapp.azure.com/',
-    provider: 'http://housing.southcentralus.cloudapp.azure.com/'
+    account: 'https://account-aspnet-dev.azurewebsites.net/',
+    complex: 'https://complex-aspnet-dev.azurewebsites.net/',
+    tenant: 'https://tenant-aspnet-dev.azurewebsites.net/',
+    provider: 'https://housing-angular-dev.azurewebsites.net/'
   }
 };
