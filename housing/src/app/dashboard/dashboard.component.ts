bf0a94fc494ea0c2324df7fc65c1949488f4c83f
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2020-02-28 13:36:27 -0600

    updated dashboard to route components and show them in the page

--- housing/src/app/dashboard/dashboard.component.ts
+++ housing/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core'; 
 
 @Component({
   selector: 'dev-dashboard',
@@ -7,22 +6,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  toggleVisible(selector:string){
-    //check if only one is toggled
-    document.querySelectorAll(".display-item").forEach(function(e){
-      e.classList.add("hide");
-    });
-
-    if(document.querySelector(selector).classList.contains("hide"))
-    {
-      document.querySelector(selector).classList.remove("hide");
-    }
-  }
-
+  constructor(
+  ) { }
+  
   ngOnInit() {
   }
 
+  addClass(selector:string,selectorClass:string)
+  {
+    document.querySelector(selector).classList.add(selectorClass);
+  }
+  removeClass(selector:string,selectorClass:string)
+  {
+    document.querySelector(selector).classList.remove(selectorClass);
+  }
+  toggleDropdown(){
+    //check if only one is toggled  
+    this.toggleActive("#complex-nav");
+    this.addClass("#complex-nav","sub-active");
+
+    if(document.querySelector("#dropdown-container").classList.contains("hide"))
+    {
+      this.removeClass("#dropdown-container","hide"); 
+    }
+    else{ 
+      this.addClass("#dropdown-container","hide");
+    }
+  } 
+  toggleActive(selector:string)
+  {
+    //if any item is clicked remove active style from all, then add active to the one you want
+    //but you don't want to remove from drop head when those sub components get selected
+    document.querySelectorAll(".dashboard-nav-item").forEach(function(e){
+      e.classList.remove("active");
+    });
+
+    if(document.querySelector(selector).classList.contains("main"))
+    {
+      this.addClass("#dropdown-container","hide");
+      this.removeClass("#complex-nav","sub-active");
+    }
+
+    document.querySelector(selector).classList.add("active");
+  }
 }
