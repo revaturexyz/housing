d7feafe28b66f36daea0a86f08f78ecd783c8ab7
filakj <john.m.filak@gmail.com>
2020-02-28 11:11:53 -0600

    address service created, get and post method for address added

--- housing/src/app/services/address.service.ts
+++ housing/src/app/services/address.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Address } from 'src/interfaces/address'
+import { PostAddress} from 'src/interfaces/post-address'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AddressService {
 
-  constructor() { }
+
+export class AddressService {
+  apiUrl: string = `${environment.endpoints.tenant}` + 'api/address/';
+
+  httpOptions: any;
+
+  constructor(
+    private httpBus: HttpClient
+  ) {
+    this.httpOptions = {
+      headers: new HttpHeaders({})
+    };
+  }
+
+  
+  
+  /*Method: Get Address By ID  
+  * Inputs: address id - type string , exprected GUID, to send to API at api/address/{addressId}
+  * Description from the API, this would send a get request with the address ID 
+  */
+  GetAddressById(AddressId: string): Observable<any> {
+    const addressUrl = `${this.apiUrl}` + `${AddressId}`;
+    return this.httpBus.get<Address>(addressUrl, this.httpOptions);
+  }
+
+
+
+  /*Method: Post Address
+  * Inputs: Address by the interface PostAddress 
+  * API DOES NOT HAVE METHOD FOR POSTING ADDRESS 
+  * THIS WOULD NEED TO TAKE ALL OTHER INPUTS FROM THE POST ADDRESS INTERFACE 
+  * AND GENERATE THE GUID TO POST THE NEW ADDRESSS 
+  * 
+  * THIS IS A PLACE HOLDER FOR METHOD 
+  */
+ PostAddress(postAddress: PostAddress): Promise<PostAddress> {
+  const postAddressUrl = `${this.apiUrl}` + 'Address/';
+  return this.httpBus.post<PostAddress>(postAddressUrl, postAddress).toPromise();
+}
+
+
 }
